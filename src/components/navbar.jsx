1f181e974f6aa9c5c5f1913d0dac73448d8d5b29
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-05-03 00:24:46 +0600

    added new styles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">Navbar</div>;
 };
 
 export default Navbar;
